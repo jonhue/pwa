35ec20657fe036a711ed2522f26b0a5db5c0bb76
Jonas Hübotter <jonas.huebotter@gmail.com>
2018-03-27 19:13:46 +0200

    bump version to 4.0.3

--- lib/pwa/version.rb
+++ lib/pwa/version.rb
@@ -1,5 +1,5 @@
 module Pwa
 
-    VERSION = '4.0.2'
+    VERSION = '4.0.3'
 
 end
--- src/pwa-rails.js
+++ src/pwa-rails.js
@@ -5,7 +5,7 @@ class ProgressiveWebApp {
         if (navigator.serviceWorker.controller) {
             console.log('[PWA] Active service worker found')
         } else {
-            navigator.serviceWorker.register( this.serviceWorkerUrl(), { scope: './' }).then(function(reg) {
+            navigator.serviceWorker.register( this.serviceWorkerUrl, { scope: './' }).then(function(reg) {
                 console.log( '[PWA] ServiceWorker registration successful with scope: ', reg.scope );
             }, function(err) {
                 console.log( '[PWA] ServiceWorker registration failed: ', err );
@@ -21,7 +21,7 @@ class ProgressiveWebApp {
     }
 
     get serviceWorkerUrl() {
-        return getBaseUrl() + this.serviceWorkerPath();
+        return getBaseUrl() + this.serviceWorkerPath;
     }
 
 }
